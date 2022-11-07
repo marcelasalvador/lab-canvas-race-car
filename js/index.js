0a56f8a232727d9edc37cfc6683d72bac364ef3e
Marcela Salvador <salvadormarcela18@gmail.com>
2022-11-06 21:15:50 -0600

    draft

--- js/index.js
+++ js/index.js
@@ -3,5 +3,16 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+
+  let canvas = document.getElementById("canvas");
+  if (canvas.style.display === "none") {
+    canvas.style.display = "block";
+  } else {
+    canvas.style.display = "none";
+  }
+  return startGame
+  }
 };
+
+
